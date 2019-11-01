11c4feb6991f150fa93ea9ff09c6d889115ce7d7
jacobmikehyde <tomaslgravel@gmail.com>
2019-11-01 16:30:46 -0400

    comment out sqlite

--- papaginos/settings.py
+++ papaginos/settings.py
@@ -90,17 +90,17 @@ DATABASES = {
         "PASSWORD": "",
         "HOST": "localhost",
         "PORT": "",
-    }
+    }}
 
     
     #this is for local 
-    """
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-    }"""
-    }
+           
+            #'default': {
+               # 'ENGINE': 'django.db.backends.sqlite3',
+                #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            #}
+           # }"""
+
 
 
 # Password validation

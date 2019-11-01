40e4b934c8630bef7716fa22a021b8b077de55b2
jacobmikehyde <tomaslgravel@gmail.com>
2019-11-01 16:21:34 -0400

    added django-heroku

--- papaginos/settings.py
+++ papaginos/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -155,3 +155,5 @@ EMAIL_HOST_USER = 'j.wyman328'
 EMAIL_HOST_PASSWORD = 'braves328'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+django_heroku.settings(locals())

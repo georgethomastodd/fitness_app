0f73f6844deb33a553daa7818c16df0c8bb903e8
jacobmikehyde <tomaslgravel@gmail.com>
2019-11-01 17:00:38 -0400

    last

--- papaginos/settings.py
+++ papaginos/settings.py
@@ -86,11 +86,6 @@ DATABASES = {
 
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "",
-        "HOST": "localhost",
-        "PORT": "5432",
     }}
 
     

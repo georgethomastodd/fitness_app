5407925920639071e0a8ed7eea2cfe605cb7a819
jacobmikehyde <tomaslgravel@gmail.com>
2019-11-01 14:11:36 -0400

    removed db

--- papaginos/settings.py
+++ papaginos/settings.py
@@ -83,10 +83,17 @@ WSGI_APPLICATION = 'papaginos.wsgi.application'
 
 DATABASES = {
     'default': {
+        
+    }
+    
+    #this is for local 
+    """
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+    }"""
+    }
 
 
 # Password validation

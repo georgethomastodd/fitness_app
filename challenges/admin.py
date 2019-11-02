8f0395d542c61e49afd0bd6a8ccafef7ee1eb070
jacobmikehyde <tomaslgravel@gmail.com>
2019-11-02 10:29:39 -0400

    challenge visible in admin

--- challenges/admin.py
+++ challenges/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Challenge, Invitation_to_challenge, Invitation_status
+admin.site.register(Challenge)
+admin.site.register(Invitation_to_challenge)
+admin.site.register(Invitation_status)
+
